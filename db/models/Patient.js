14d1aa07b24ec309346a3a0c27b3d4af0e1ad321
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-09-07 12:25:43 -0300

    remove weekday enum

--- db/models/Patient.js
+++ db/models/Patient.js
@@ -37,7 +37,7 @@ const PatientSchema = new mongoose.Schema({
   },
   weekday: {
     type: String,
-    enum: weekdays,
+    // enum: weekdays,
     default: defaultWeekday(new Date().getDay()),
     trim: true
   },
